43d6f0b2c4c567e10f9c6f891cd6d38d024a22a0
Ian Leue <ian@appboy.com>
2015-02-26 15:00:38 -0500

    Restore rails 3 support

--- lib/requirejs/rails/view_proxy.rb
+++ lib/requirejs/rails/view_proxy.rb
@@ -2,7 +2,7 @@ module Requirejs
   module Rails
     class ViewProxy
       include ActionView::Context
-      include ActionView::Helpers::AssetUrlHelper
+      include ActionView::Helpers::AssetTagHelper
       include ActionView::Helpers::TagHelper
     end
   end
